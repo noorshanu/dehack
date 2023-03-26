fb13a5bef8a4b3b54fecd314c80770024e002c71
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-03-27 03:14:37 +0530

    Update OurVision.js

--- elements/AboutHero/OurVision.js
+++ elements/AboutHero/OurVision.js
@@ -25,7 +25,6 @@ const OurVision = ({ images, width }) => {
     },
   }
 
-
   const SecondBox = {
     loop: true,
     autoplay: true,
@@ -34,7 +33,6 @@ const OurVision = ({ images, width }) => {
       preserveAspectRatio: 'xMidYMid slice',
     },
   }
-
 
   const ThirdBox = {
     loop: true,
@@ -51,13 +49,13 @@ const OurVision = ({ images, width }) => {
       <Container className="relative">
         <div className="stats_container flex flex-col gap-14 items-center bg-[#111111] p-14 lg:p-24 lg:py-24 xl:py-16 text-white mx-auto !rounded-[30px] relative">
           <div className="flex flex-col gap-10 lg:gap-14 items-center">
-            <h3 className="font-[250] px-14 md:px-0 text-3xl md:text-7xl text-center spacing">
+            <h3 className="font-[250] px-14 md:px-0 text-3xl md:text-5xl text-center spacing">
               Our <span className="font-normal">Vision</span>
             </h3>
-            <div className=" w-full max-w-[800px] line"></div>
+            <div className=" w-full max-w-[600px] line"></div>
             {width >= 768 && (
               <div className="hidden md:grid grid-cols-3 works-wrapper md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10 gap-y-20 mt-10">
-                <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
+                <div className="gradient-border  p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[320px]">
                   <Lottie
                     style={{ margin: 0 }}
                     options={ThirdBox}
@@ -72,11 +70,8 @@ const OurVision = ({ images, width }) => {
                     world.
                   </p>
                 </div>
-               
 
-
-
-                <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
+                <div className="gradient-border  p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[320px]">
                   <Lottie
                     style={{ margin: 0 }}
                     options={SecondBox}
@@ -85,10 +80,12 @@ const OurVision = ({ images, width }) => {
                   />
 
                   <p className="text-xs md:text-base 2xl:text-xl font-light md:font-light tracking-[1px]">
-                  The ethos of blockchain technology is community, and we are working towards making sure that this community is safe for everyone to be a part of.
+                    The ethos of blockchain technology is community, and we are
+                    working towards making sure that this community is safe for
+                    everyone to be a part of.
                   </p>
                 </div>
-                <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
+                <div className="gradient-border  p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[320px]">
                   <Lottie
                     style={{ margin: 0 }}
                     options={defaultOptions}
@@ -98,7 +95,9 @@ const OurVision = ({ images, width }) => {
                   />
 
                   <p className="text-xs md:text-base 2xl:text-xl font-light md:font-light tracking-[1px]">
-                  We believe that security concerns are the primary reason for slow growth of web3 adopters and we are working to create a solution to that problem.
+                    We believe that security concerns are the primary reason for
+                    slow growth of web3 adopters and we are working to create a
+                    solution to that problem.
                   </p>
                 </div>
               </div>
@@ -113,7 +112,7 @@ const OurVision = ({ images, width }) => {
                   className="mySwiper max-w-full"
                 >
                   <SwiperSlide className="!grid grid-cols-1 gap-4"> */}
-                  <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
+                <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
                   <Lottie
                     style={{ margin: 0 }}
                     options={ThirdBox}
@@ -128,9 +127,6 @@ const OurVision = ({ images, width }) => {
                     world.
                   </p>
                 </div>
-               
-
-
 
                 <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
                   <Lottie
@@ -141,15 +137,13 @@ const OurVision = ({ images, width }) => {
                   />
 
                   <p className="text-xs md:text-base 2xl:text-xl font-light md:font-light tracking-[1px]">
-                  The ethos of blockchain technology is community, and we are working towards making sure that this community is safe for everyone to be a part of.
+                    The ethos of blockchain technology is community, and we are
+                    working towards making sure that this community is safe for
+                    everyone to be a part of.
                   </p>
                 </div>
 
-
-
-
-
-                 <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
+                <div className="p-4 py-6 md:p-5 md:py-8 2xl:p-8 text-white border border-white bg-[#111111] flex flex-col gap-5 rounded-lg md:rounded-[20px] w-full md:w-[240px] 2xl:w-[280px]">
                   <Lottie
                     style={{ margin: 0 }}
                     options={defaultOptions}
@@ -159,11 +153,13 @@ const OurVision = ({ images, width }) => {
                   />
 
                   <p className="text-xs md:text-base 2xl:text-xl font-light md:font-light tracking-[1px]">
-                  We believe that security concerns are the primary reason for slow growth of web3 adopters and we are working to create a solution to that problem.
+                    We believe that security concerns are the primary reason for
+                    slow growth of web3 adopters and we are working to create a
+                    solution to that problem.
                   </p>
                 </div>
-                
-                  {/* </SwiperSlide>
+
+                {/* </SwiperSlide>
                 </Swiper> */}
                 {/* <div className="flex justify-center gap-2">
                   <button className="prev rotate-180 transitions" ref={prevRef}>
