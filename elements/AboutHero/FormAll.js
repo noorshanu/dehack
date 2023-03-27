7fa3b33a038670bdac532e43fc620bc6bbde78cc
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-03-27 22:51:22 +0530

    Update FormAll.js

--- elements/AboutHero/FormAll.js
+++ elements/AboutHero/FormAll.js
@@ -1,14 +1,74 @@
-import React from 'react'
+import React,{useState} from 'react'
+
+
 
 function FormAll() {
+    const [userData, setUserData] = useState({
+        yourName: "",
+        CompanyName: "",
+        
+        email: "",
+        
+        message: "",
+      });
+    
+      let name, value;
+      const postUserData = (event) => {
+        name = event.target.name;
+        value = event.target.value;
+    
+        setUserData({ ...userData, [name]: value });
+      };
+    
+      // connect with firebase
+      const submitData = async (event) => {
+        event.preventDefault();
+        const { yourName, companyName,  email, message } = userData;
+    
+        if (yourName && companyName && email  && message) {
+          const res = fetch(
+            "https://dehack-conatct-default-rtdb.firebaseio.com/userDataRecords.json",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                yourName,
+                companyName,
+                email,
+                message,
+              }),
+            }
+          );
+    
+          if (res) {
+            setUserData({
+              yourName: "",
+              comapnyName: "",
+              email: "",
+              message: "",
+            });
+            alert("Data Stored");
+          } else {
+            alert("plz fill the data");
+          }
+        } else {
+          alert("plz fill the data");
+        }
+      };
+
   return (
    <>
-   <form className="flex flex-col gap-4 w-full max-w-[600px] mt-10">
+   <form className="flex flex-col gap-4 w-full max-w-[600px] mt-10" method='POST'>
             <label className="text-[#fff] text-start">Your Name*</label>
             <input
               className="p-3 rounded-md outline-none border-none pl-4 text-black mb-8"
               placeholder="Your Name"
               type="text"
+              name='yourName'
+              value={userData.yourName}
+              onChange={postUserData}
             />
 
             <label className="text-[#fff] text-start">Company Name*</label>
@@ -16,6 +76,9 @@ function FormAll() {
               className="p-3 rounded-md outline-none border-none pl-4 text-black mb-8"
               placeholder="Company Name"
               type="text"
+              name='companyName'
+              value={userData.companyName}
+              onChange={postUserData}
             />
 
             <label className="text-[#fff] text-start">Email*</label>
@@ -23,15 +86,22 @@ function FormAll() {
               className="p-3 rounded-md outline-none border-none pl-4 text-black mb-8"
               placeholder="Email"
               type="email"
+              name='email'
+              value={userData.email}
+              onChange={postUserData}
             />
             <label className="text-[#fff] text-start">Message*</label>
             <textarea
               class="resize-y p-3 rounded-md outline-none border-none pl-4 text-black h-[200px]"
               placeholder="Message"
+              name='message'
+              value={userData.message}
+              onChange={postUserData}
             ></textarea>
             <button
-              type="button"
-              onClick={() => alert('Hello')}
+              type="submit"
+              onClick={submitData}
+              
               className="px-10 md:w-[150px] h-[45px] border-[3px] mx-auto md: border-white rounded-md mt-auto text-white"
             >
               Submit
