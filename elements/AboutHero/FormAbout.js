b799407bcd9c4a2e47b78dc13a87e1604ea0ab83
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-03-30 00:48:13 +0530

    Create FormAbout.js

---
+++ elements/AboutHero/FormAbout.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+
+function FormAbout() {
+    const [email, setEmail] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+  
+    const [userData, setUserData] = useState({
+      yourName: '',
+      companyName: '',
+      email: '',
+      message: '',
+    })
+  
+    const postUserData = (event) => {
+      const { name, value } = event.target;
+      setUserData({ ...userData, [name]: value });
+  
+      if (name === "email") {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(value)) {
+          setErrorMsg("Please enter a valid email address.");
+        } else {
+          setErrorMsg("");
+        }
+      }
+    }
+  
+    const submitData = async (event) => {
+        event.preventDefault()
+        const { yourName, companyName, email, message } = userData
+      
+        if (yourName && companyName && email && message && !errorMsg) {
+          const res = fetch(
+            'https://dehack-conatct-default-rtdb.firebaseio.com/userDataRecords.json',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                yourName,
+                companyName,
+                email,
+                message,
+              }),
+            }
+          )
+      
+          if (res) {
+            setUserData({
+              yourName: '',
+              companyName: '',
+              email: '',
+              message: '',
+            })
+            alert(
+              'Thanks for filling the form. We will get back to you in appropriate time.'
+            )
+          } else {
+            alert('Please fill the right information')
+          }
+        } else {
+          alert('Please fill the right information')
+        }
+      }
+  
+
+  return (
+    <>
+      <form
+        className="flex flex-col gap-4 w-full max-w-[600px] mt-10 p-5 sm:p-0"
+        method="POST"
+      >
+       
+        <input
+          className="p-3 rounded-md outline-none border-none pl-4 text-black mb-8"
+          
+          type="text"
+          name="yourName"
+          value={userData.yourName}
+          onChange={postUserData}
+          required
+        />
+
+      
+   
+
+     
+        <input
+          className="p-3 rounded-md outline-none border-none pl-4 text-black mb-8"
+          
+          id='email'
+          type="email"
+          name="email"
+          value={userData.email}
+          onChange={postUserData}
+        />
+         {errorMsg && <div className="error text-white">{errorMsg}</div>}
+     
+        <textarea
+          className="resize-y p-3 rounded-md outline-none border-none pl-4 text-black h-[200px]"
+         
+          type='text'
+          name="message"
+          value={userData.message}
+          onChange={postUserData}
+        
+        ></textarea>
+        <button
+          type="submit"
+          onClick={submitData}
+          className="px-10 md:w-[150px] h-[45px] border-[3px] mx-auto md: border-white rounded-md mt-auto text-white"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  )
+}
+
+export default FormAbout
