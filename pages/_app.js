e166d55918348497a359a1be761f7a5a66be9ee4
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-04-09 03:57:54 +0500

    fixed loader issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,26 @@
 import '@/styles/globals.css'
 import Loader from '@/public/loader.svg'
 import Image from 'next/image'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
+
 export default function App({ Component, pageProps }) {
-  const [loading,setLoading] =useState(false)
-  useEffect(()=>{
-    setLoading(true)
-    setTimeout(()=>{
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
       setLoading(false)
-    },3000)
-  },[])
+    }, 3000)
+  }, [])
+
   return (
-  <>
-   {
-      loading ?
-      <div className='bg-black flex justify-center loader'>
-        <Image src={Loader} alt=''/>
-      </div>
-      :<Component {...pageProps} />
-   }
-  </>
+    <>
+      {loading ? (
+        <div className="bg-black flex justify-center loader">
+          <Image src={Loader} alt="" />
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
   )
 }
